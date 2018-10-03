4b34b920ae28c13ffbb3ea03ada141bd55f70671
[mediba] Fumiyuki Matsushima <f-matsushima@mediba.jp>
2018-10-03 10:54:31 +0900

    Revert "リファクタリング"
    This reverts commit 5d8f8387029ed7970c0eb4a6f1eaf678a7ec0fa2.

---
+++ Prism.NavigationEx/INavigationPath.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Prism.Navigation;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Prism.NavigationEx
+{
+    public interface INavigationPath
+    {
+        (string Path, NavigationParameters Parameters) GetPathAndParameters(NavigationParameters additionalParameters = null, NavigationParameters additionalPathParameters = null);
+    }
+
+    public interface INavigationPath<TRootViewModel> : INavigationPath where TRootViewModel : INavigationViewModel
+    {
+    }
+}
